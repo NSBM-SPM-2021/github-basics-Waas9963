a6707e051e4b1807dc6880bcca76a9ed93005d67
Waas9963 <wasananarasinghe63@gmail.com>
2021-09-15 11:39:34 +0530

    Updates

--- src/app/components/parts-cart/cart/cart.component.ts
+++ src/app/components/parts-cart/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MessengerService } from 'src/app/services/messenger.service';
+import { MessengerService } from 'src/app/services/messenger.service'
+
 
 @Component({
   selector: 'app-cart',
@@ -8,21 +9,17 @@ import { MessengerService } from 'src/app/services/messenger.service';
 })
 export class CartComponent implements OnInit {
 
-  cartItems=[
-    {id: 1,productId:1,productName:'Rods',qty:3,price:1000},
-    {id: 2,productId:2,productName:'Bolts',qty:4,price:600},
-    {id: 3,productId:3,productName:'Pistons',qty:2,price:900},
-    {id: 4,productId:4,productName:'Bearing kit',qty:3,price:1000},
-    {id: 5,productId:5,productName:'Ball joint',qty:1,price:800},
-   ];
-   cartTotal=0
+  cartItems = [
+    ];
 
-   constructor(private msg: MessengerService){}
-  ngOnInit(){
-    this.cartItems.forEach(item=>{
-      this.cartTotal +=(item.qty * item.price)
+  cartTotal = 0
 
-    })
+  constructor(private msg: MessengerService) { }
+
+  ngOnInit() {
+    this.cartTotal = 0
+    
+    }
   }
 
-}
+
