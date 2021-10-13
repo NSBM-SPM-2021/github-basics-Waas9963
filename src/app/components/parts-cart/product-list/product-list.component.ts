e443c9feaf783f9aa23945a36b534626ff8ccea0
Waas9963 <wasananarasinghe63@gmail.com>
2021-10-13 13:43:13 +0530

    wishlist functionality

--- src/app/components/parts-cart/product-list/product-list.component.ts
+++ src/app/components/parts-cart/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service'
 import { Product } from 'src/app/models/product';
+import { WishlistService } from 'src/app/services/wishlist.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,13 +11,21 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
 
   productList: Product[] = []
+  wishlist:number[]=[]
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private wishlistService:WishlistService) { }
 
   ngOnInit() {
+    this.loadProducts();
+  }
+  loadProducts(){
     this.productService.getProducts().subscribe((products)=>{
       this.productList=products;
   })
 
+}
+loadWishlist(){
+  this.wishlistService.getWishlist().subscribe(productIds=> this.wishlist= productIds)
 }
 }
--- src/app/services/wishlist.service.ts
+++ src/app/services/wishlist.service.ts
@@ -11,11 +11,20 @@ export class WishlistService {
   constructor(private http: HttpClient) { }
 
   getWishlist(){
-    return this.http.get(wishlistUrl).pipe()
+    return this.http.get(wishlistUrl).pipe(
+      map((result:any)=>{
+        let productIds: any[]=[]
+         
+        result.forEach((item: { id: any; })=> productIds.push(item.id))
+
+
+        return productIds;
+      })
+    )
  
   }
 
-  addToWishlist(productId: any){
+  addToWishlist(productId:any){
      return this.http.post(wishlistUrl,{id:productId})
   }
 
