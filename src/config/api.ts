00827ccc972e9fa9af201baf26486b4b974bfb6e
Waas9963 <wasananarasinghe63@gmail.com>
2021-10-13 21:06:57 +0530

    indexing

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import { environment } from 'src/environments/environment'
 
-export const baseUrl = environment.production ? 'https://parts-cart.web.app/' :'http://localhost:3000'
+export const baseUrl = environment.production? 'http://localhost:4200/' :'http://localhost:3000'
 export const productsUrl=baseUrl + '/products'
 export const cartUrl=baseUrl + '/cart'
 export const wishlistUrl= baseUrl +'/wishlist'
